20d00ac49d902444b08d70527297277bfd3b5b20
kokily <kokily@naver.com>
2025-08-17 13:34:51 +0900

    Backend Project init

---
+++ server/src/main.ts
@@ -0,0 +1,54 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Cors
+  app.enableCors();
+
+  // Global Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // API Routes
+  app.setGlobalPrefix('api');
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('행사전표시스템 API')
+    .setDescription('행사전표시스템 v6.0 API Document')
+    .setVersion('6.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT Token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || '3000';
+
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger docs: http://localhost:${port}/api/docs`);
+}
+
+bootstrap();
