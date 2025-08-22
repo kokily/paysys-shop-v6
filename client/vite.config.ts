e376c252dc07373dc85ce27e5399588ea8d56680
kokily <kokily@naver.com>
2025-08-22 13:40:14 +0900

    Client Sass Style config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "${path.resolve(__dirname, 'src/styles/variables')}" as *; @use "${path.resolve(
+          __dirname,
+          'src/styles/mixins'
+        )}" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+});
